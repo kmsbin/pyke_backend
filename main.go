a779ae22f06f586934fe3d9c17ab5da9c8d286f4
kauli-sabino <kaulisabino27@gmail.com>
2021-01-19 17:38:47 -0300

    try deploy heroku

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	log.Print("Server rodando")
+	log.Print("Server rodando liso")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print("Bateu na rota '/'")
 		w.Write([]byte("horário oficial do oleo de macaco: " + time.Now().Format("15:04:05 02-01-2006")))
