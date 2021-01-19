549fc488781eae5e10ba7fa49769284eb110cdbe
kauli-sabino <kaulisabino27@gmail.com>
2021-01-19 18:58:21 -0300

    fix port error

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -12,5 +13,5 @@ func main() {
 		log.Print("Bateu na rota '/'")
 		w.Write([]byte("horário oficial do oleo de macaco: " + time.Now().Format("15:04:05 02-01-2006")))
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(string(":"+os.Getenv("PORT")), nil))
 }
