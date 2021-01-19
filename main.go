636d715c34406159c208821cea7bbc3e072d8a42
kauli-sabino <kaulisabino27@gmail.com>
2021-01-19 15:47:17 -0300

    refresh heroku

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	log.Print("server rodando")
+	log.Print("Server rodando")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print("Bateu na rota '/'")
 		w.Write([]byte("horário oficial do oleo de macaco: " + time.Now().Format("15:04:05 02-01-2006")))
